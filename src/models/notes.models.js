6a0f961e0c3772c5360017b1792ed7f4d6e36353
Harsh <haras9451@gmail.com>
2026-05-29 00:44:22 +0530

    User Model is made

---
+++ src/models/notes.models.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+let noteSchema = new mongoose.Schema({
+    title : String,
+    description: String
+})
+
+
+const NoteModel = mongoose.model('notes', noteSchema)
+
+module.exports = NoteModel;
