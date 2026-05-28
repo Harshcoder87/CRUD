5ef22f147777d240376e30555150e87a1df783e5
Harsh <haras9451@gmail.com>
2026-05-28 17:15:04 +0530

    App and server file made

---
+++ server.js
@@ -0,0 +1,25 @@
+
+import app from "./src/app.js"
+
+app.listen(3000, () =>{
+    console.log("Server is running at port 3000");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//for now server start aim and connecting with db
---
+++ src/app.js
@@ -0,0 +1,19 @@
+
+import express from 'express';
+
+const app = express();
+
+
+export default app;
+
+
+
+
+
+
+
+
+
+// server instance creation and config krna
+
+//for server config and creationlike - logger, morgan, middleware, rate limiting
